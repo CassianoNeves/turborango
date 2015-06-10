f2573e22b2ff106cb479512b470791db2409f672
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-06-10 17:23:58 -0300

    Acompanhamento da aula

--- src/TurboRango/TurboRango.Web/App_Start/BundleConfig.cs
+++ src/TurboRango/TurboRango.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace TurboRango.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/haversine").Include(
+                        "~/Scripts/haversine.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
